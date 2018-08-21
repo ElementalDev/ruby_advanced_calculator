59860acb83dbe79714748852b0c83f3cc3174651
Reece_Collett <rcollett@spartaglobal.com>
2018-08-21 17:06:28 +0100

    Initial Commit

---
+++ lib/calc_engine.rb
@@ -0,0 +1,28 @@
+class CalcEngine
+
+  def run_calculator
+    user_choice = ""
+    number1 = 0
+    number2 = 0
+    # Basic menu
+    puts "---------------------------------------"
+    puts "Which calculator would you like to use?"
+    puts "(Add)ition"
+    puts "(Sub)traction"
+    puts "(Mul)tiplication"
+    puts "(Div)ision"
+    puts "---------------------------------------"
+    user_choice = gets.chomp
+    user_choice.downcase
+
+  end
+
+  # Adds 2 numbers
+  def add_two_numbers(num1, num2)
+    num1 + num2
+  end
+  
+end
+
+calc = CalcEngine.new()
+calc.run_calculator
